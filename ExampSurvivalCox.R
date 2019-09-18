7386610f182d74da68105a164f11cb22e82ce320
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2019-09-18 17:26:24 -0500

    Update ExampSurvivalCox.R
    Some ggplot type survival plots

--- ExampSurvivalCox.R
+++ ExampSurvivalCox.R
@@ -1,13 +1,15 @@
 # Example from Glioma Radioimmunotherapy
 # Chapter 11 HSAUR 3
 #
-
+library("tidyverse")
+library("car")
+library("survival")
+library("coin")
 
 data("glioma", package = "coin")
 
 # first we split the screen in two
 layout(matrix(1:2, ncol=2))
-
 g3 <- subset(glioma, histology == "Grade3")
 g4 <- subset(glioma, histology == "GBM")
 
@@ -24,25 +26,80 @@ survdiff(Surv(time, event) ~ group, data = g3)
 # More interesting question is the novel Radioimmunotherapy is superior for both groups
 # is done stratifying od blocking (by block) with respect to tumor grading 
 # defunct surv_test(Surv(time, event) ~ group | histology, data = glioma, distribution = approximate(B = 10000))
-
 # probability estimation with exact distribution
 logrank_test(Surv(time, event) ~ group, data = g3, distribution = "exact")
 logrank_test(Surv(time, event) ~ group, data = g4, distribution = "exact")
 # To answer the question whether the survival times differ for grade III patients 
 logrank_test(Surv(time, event) ~ group | histology, data = glioma, distribution = approximate(nresample = 10000L))
 
+ggsurvplot(
+  survfit(Surv(time, event) ~ group, data = g3), 
+  data = g3, 
+  size = 0.5,                      # change line size
+  linetype = c("dashed", "solid"), # different line type
+  palette = "lancet",              # color red, blue or custom palettes lancet
+  title   = "Grade III Glioma",    # plot main title
+  xlab = "Survival in Months",     # customize X axis label.
+  conf.int = TRUE,                 # Add confidence interval
+  pval = TRUE,                     # Add p-value from log-rank test
+  risk.table = TRUE,               # Add risk table
+  risk.table.col = "strata",       # Risk table color by groups
+  legend.labs = c("Control","RIT"),# Change legend labels
+  risk.table.height = 0.25,        # Useful to change when you have multiple groups
+  surv.median.line = "hv",         # add the median survival pointer.
+  ggtheme = theme_bw()             # Change ggplot2 theme
+)
+
+# Other survival plots with ggplot
+ggsurvplot(
+survfit(Surv(time, event) ~ group, data = g4), 
+data = g4, 
+size = 0.5,                  # change line size
+linetype = c("dashed", "solid"), # different line type
+palette = "lancet",          # color red, blue or custom palettes lancet
+title   = "Grade IV Glioma", # plot main title
+xlab = "Survival in Months", # customize X axis label.
+conf.int = TRUE,             # Add confidence interval
+pval = TRUE,                 # Add p-value from log-rank test
+risk.table = TRUE,           # Add risk table
+risk.table.col = "strata",   # Risk table color by groups
+legend.labs = c("Control","RIT"),    # Change legend labels
+risk.table.height = 0.25,    # Useful to change when you have multiple groups
+surv.median.line = "hv",     # add the median survival pointer.
+ggtheme = theme_bw()      # Change ggplot2 theme
+)
+
 #Cox proportional-hazards regression
 #library ("carData")
 args(coxph)
 Rossi[1:5, 1:10]
-
+# Cox model and estimation of model tests
 mod.allison <- coxph(Surv(week, arrest) ~ fin + age + race + wexp + mar + paro + prio, data = Rossi)
 summary(mod.allison)
-
 # car has an ANOVA to estimate Type-II likelihood test for Cox models
 Anova(mod.allison)
+
+# Plots of the Cox model
 plot(survfit(mod.allison), xlab="Weeks", ylab="Proportion Not Rearrested")
 plot(survfit(mod.allison), ylim = c(0.7, 1), xlab="Weeks", ylab="Proportion Not Rearrested")
+
+ggsurvplot(
+survfit(mod.allison), 
+data = Rossi, 
+size = 0.5,                  # change line size
+linetype = c("dashed", "solid"), # different line type
+palette = "lancet",          # color red, blue or custom palettes lancet
+title   = "Rearrest data", # plot main title
+xlab = "Weeks", # customize X axis label.
+conf.int = TRUE,             # Add confidence interval
+pval = TRUE,                 # Add p-value from log-rank test
+risk.table = TRUE,           # Add risk table
+risk.table.col = "strata",   # Risk table color by groups
+legend.labs = c("CoxModel","Envelope"),    # Change legend labels
+risk.table.height = 0.25,    # Useful to change when you have multiple groups
+surv.median.line = "hv",     # add the median survival pointer.
+ggtheme = theme_bw()         # Change ggplot2 theme
+)
 
 # To study the fin variable
 Rossi.fin <- with (Rossi, data.frame(fin=c(0, 1), age=rep(mean(age), 2), race=rep(mean(race == "other"), 2),
@@ -51,4 +108,6 @@ Rossi.fin <- with (Rossi, data.frame(fin=c(0, 1), age=rep(mean(age), 2), race=re
 plot(survfit(mod.allison, newdata=Rossi.fin), conf.int=TRUE, lty=c(1,2), ylim=c(0.6, 1), xlab="Weeks", 
      ylab="Proportion Not Rearrested")
 legend("bottomleft", legend=c("fin - no", "fin = yes"), lty=c(1, 2), insert=0.02)
+
+
 
