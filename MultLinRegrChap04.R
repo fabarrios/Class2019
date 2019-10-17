a6bb2cf8a24f7f90bb5b11a35a0fb3efb2e6404a
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2019-10-16 19:00:33 -0500

    Update MultLinRegrChap04.R
    Added the confounding example for chap 4 and interaction and examples of correction fo linear combination of coefficients

--- MultLinRegrChap04.R
+++ MultLinRegrChap04.R
@@ -158,3 +158,106 @@ plot(predictorEffects(prestige.mod.1,
       predictors = ~ type + education + income))
 
 emmeans(prestige.mod.1, pairwise ~ type)$contrasts
+
+# Confounding 
+# when one predictor (independet variable) can depend in some of the 
+# same variables that the responce function
+# Example 4 
+# Example of BMI (Body mass index) and LDL (colesterol) using the HERS cohort data Table 4.12
+
+LDL_fit_bmi <- lm(LDL ~ BMI, data = hers)
+S(LDL_fit_bmi)
+LDL_fit_all <- lm(LDL ~ BMI + age + nonwhite + smoking + drinkany, data = hers)
+S(LDL_fit_all)
+
+# Chapter 4, Interaction
+# Now for the group with Hormone Therapy the complete HERS cohort
+# Model of cholesterol LDL and the effect of Hormone Therapy (HT) and
+# Statin use model is at the one year visit (to se the HT effect)
+hers <- mutate(hers, HT = factor(HT))
+hers <- hers %>% mutate(HT = relevel(HT, ref = "placebo"))
+
+# For the first year visit LDL levels at one year LDL1
+LDL1_model <- lm(LDL1 ~ HT * statins, data = hers)
+S(LDL1_model)
+
+# For the table 4.15 of the book, to test for the linear 
+# combination of the coefficients for the interaction to
+# TEST the linear combination of the coefficients for HT
+# and the statin interaction b1 and b3 the third part of table 4.15
+
+coefeq <- matrix(data=0, nrow = 1, ncol = length(LDL_model$coefficients))
+colnames(coefeq) <- names(LDL_model$coefficients)
+coefeq
+coefeq[1, "HThormone therapy"] <- 1
+coefeq[1, "HThormone therapy:statinsyes"] <- 1
+coefeq %*% LDL_model$coefficients
+
+# coeftest <- glht(model= LDL_model, linfct= coefeq, rhs= 0, alternative= "greater")
+coeftest <- glht(model= LDL_model, linfct= coefeq, rhs= 0)
+summary(coeftest)
+
+# Example of LDL after one year of HT and physact
+# This is the table 4.16 of the book, effect of the hormone therapy
+# combined with effects of physical activity at the one year visit
+# now to estimate the linear combination of the coefficients
+hers <- mutate(hers, physact = factor(physact, levels=c("much less active","somewhat less active","about as active","somewhat more active","much more active")))
+LDL1phys_model <- lm(LDL1 ~ HT * physact, data = hers)
+S(LDL1phys_model)
+
+# The coefficients test for interaction
+# can be done with glht()
+
+coef_LDL1phys <- matrix(data=0, nrow = 1, ncol = length(LDL1phys_model$coefficients))
+colnames(coef_LDL1phys) <- names(LDL1phys_model$coefficients)
+
+# E[LDL|x] = b0 + b1 HT + b2 physact + b3 HT:physact
+# we will look at slope = b3 BMIc; coeff = 0, 0, 0, 0, 0, 1, 1, 1, 1
+# to test of significance
+
+coef_LDL1phys[1, "HThormone therapy:physactsomewhat less active"] <- 1
+coef_LDL1phys[1, "HThormone therapy:physactabout as active"] <- 1
+coef_LDL1phys[1, "HThormone therapy:physactsomewhat more active"] <- 1
+coef_LDL1phys[1, "HThormone therapy:physactmuch more active"] <- 1
+coef_LDL1phys %*% LDLphys_model$coefficients
+coef_LDL1phys
+coef_LDL1phystest <- glht(model= LDL1phys_model, linfct= coef_LDL1phys, rhs= 0)
+S(coef_LDL1phystest)
+
+# Example of BMI and statins, with interaction
+# Model of LDL and BMI*statins and other variables
+hers <- mutate(hers, statins = factor(statins))
+hers <- mutate(hers, nonwhite = factor(nonwhite))
+hers <- mutate(hers, smoking = factor(smoking))
+hers <- mutate(hers, drinkany = factor(drinkany))
+hers <- mutate(hers, BMIc = BMI - mean(BMI, na.rm=TRUE))
+LDLbmi_model <- lm(LDL ~ statins*BMIc + age + nonwhite + smoking + drinkany, data = hers)
+summary(LDLbmi_model)
+
+# For the table 4.17 of the book, to test for the linear 
+# combination of the coefficients for the interaction to
+# TEST the linear combination of the coefficients for HT
+
+coef_LDLbmi <- matrix(data=0, nrow = 1, ncol = length(LDLbmi_model$coefficients))
+colnames(coef_LDLbmi) <- names(LDLbmi_model$coefficients)
+
+# E[LDL|x] = b0 + b1 statins + b2 BMIc + b3 statins:BMIc + b4 age
+#           + b5 nonwhite + b6 smoking + b7 + drinkany
+# we will look at slope = b2 + b3 BMIc; coeff = 0, 0, 1, 1, 0, 0, 0, 0
+
+coef_LDLbmi[1, "statinsyes"] <- 0
+coef_LDLbmi[1, "BMIc"] <- 1
+coef_LDLbmi[1, "statinsyes:BMIc"] <- 1
+coef_LDLbmi %*% LDLbmi_model$coefficients
+coef_LDLbmi
+coef_LDLbmitest <- glht(model= LDLbmi_model, linfct= coef_LDLbmi, rhs= 0)
+summary(coef_LDLbmitest)
+
+# Comp of regular linear model and robust and some standard error handling
+#
+glucose_model <- lm(glucose ~ diabetes + BMI + age + drinkany, data= hers)
+
+# MASS rlm()
+glucose_rmodel <- rlm(glucose ~ diabetes + BMI + age + drinkany, data= hers)
+#
+glucose_rmodel_bi <- rlm(glucose ~ diabetes + BMI + age + drinkany, psi= psi.bisquare, data= hers)
